60eb080cfcec449305f3d917c6568dbdf7195f9c
jaatni123 <157571705+jaatni123@users.noreply.github.com>
2024-02-27 11:54:29 +0530

    Add files via upload

---
+++ Calculator.js
@@ -0,0 +1,17 @@
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+function calculateResult() {
+    const expression = document.getElementById('display').value;
+    try {
+        const result = eval(expression);
+        document.getElementById('display').value = result;
+    } catch (error) {
+        document.getElementById('display').value = 'Error';
+    }
+}
